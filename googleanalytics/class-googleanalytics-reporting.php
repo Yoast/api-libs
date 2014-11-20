bea4ed375e3e7bdf4cae6289665d4698662bf883
Peter van Wilderen <peter@yoast.com>
2014-11-20 10:09:04 +0100

    Google analytics instance called

--- googleanalytics/class-googleanalytics-reporting.php
+++ googleanalytics/class-googleanalytics-reporting.php
@@ -10,20 +10,68 @@ class Yoast_Googleanalytics_Reporting {
 	private static $instance = null;
 
 	/**
-	 * Get instance, construct itself
+	 * Getting the instance object
 	 *
-	 * @param $name
+	 * This method will return the instance of itself, if instance not exists, becauses of it's called for the first
+	 * time, the instance will be created.
 	 *
-	 * @return bool
+	 * @return null|Yoast_Google_Analytics
 	 */
-	public function get_instance( $name ) {
-		if ( isset( self::$instances[$name] ) ) {
-			return self::$instances[$name];
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
 		}
 
-		return false;
+		return self::$instance;
 	}
 
+	/**
+	 * Doing request to Google Analytics
+	 *
+	 * This method will do a request to google and get the response code and body from content
+	 *
+	 * @param string $target_url
+	 * @param string $scope
+	 * @param string $access_token
+	 * @param string secret
+	 *
+	 * @return array|null
+	 */
+	public function do_request( $target_url, $scope, $access_token, $secret ) {
+		$gdata     = $this->get_gdata( $scope, $access_token, $secret );
+		$response  = $gdata->get( $target_url );
+		$http_code = wp_remote_retrieve_response_code( $response );
+		$response  = wp_remote_retrieve_body( $response );
+
+		if ( $http_code == 200 ) {
+			return array(
+				'response' => array( 'code' => $http_code ),
+				'body'     => $response,
+			);
+		}
+	}
+
+	/**
+	 * Getting WP_GData object
+	 *
+	 * If not available include class file and create an instance of WP_GDAta
+	 *
+	 * @param string $scope
+	 * @param null   $token
+	 * @param null   $secret
+	 *
+	 * @return WP_GData
+	 */
+	protected function get_gdata( $scope, $token = null, $secret = null ) {
+		$args = array(
+			'scope'              => $scope,
+			'xoauth_displayname' => 'Google Analytics by Yoast',
+		);
+
+		$gdata = new WP_GData( $args, $token, $secret );
+
+		return $gdata;
+	}
 
 
 }
