04937aa2d83649b5e305cad3f386a8be73986774
Peter van Wilderen <peter@yoast.com>
2014-12-01 14:59:33 +0100

    Implement repsonse changes in format

--- googleanalytics/class-googleanalytics-reporting.php
+++ googleanalytics/class-googleanalytics-reporting.php
@@ -94,10 +94,18 @@ class Yoast_Googleanalytics_Reporting {
 		if ( isset( $raw_data->rows ) ) {
 			if ( is_array( $raw_data->rows ) ) {
 				foreach ( $raw_data->rows as $key => $item ) {
-					$data[] = array(
-						'date'  => (int) $this->format_ga_date( $item[0] ),
-						'value' => (int) $item[1],
-					);
+					if ( isset( $item[2] ) ) {
+						$data[] = array(
+							'date'  => (int) $this->format_ga_date( $item[0] ),
+							'bool'  => (string) $item[1],
+							'value' => (int) $item[2],
+						);
+					} else {
+						$data[] = array(
+							'date'  => (int) $this->format_ga_date( $item[0] ),
+							'value' => (int) $item[1],
+						);
+					}
 				}
 			}
 		}
