5d5ceec11ff382f6170260c545c99bee462f0d23
Andy Meerwaldt <andy@yoast.com>
2015-07-13 14:58:11 +0200

    removed some classes

--- class-api-google.php
+++ class-api-google.php
@@ -23,41 +23,32 @@ class Yoast_Api_Google {
 			'yoast_api_google_client'      => 'class-api-google-client',
 
 			// Requires in classes
-			'yoast_google_model'           => 'google/service/Google_Model',
-			'yoast_google_service'         => 'google/service/Google_Service',
-			'yoast_google_serviceresource' => 'google/service/Google_ServiceResource',
+			'yoast_google_auth'            => 'google/auth/Google_Auth',
 			'yoast_google_assertion'       => 'google/auth/Google_AssertionCredentials',
 			'yoast_google_signer'          => 'google/auth/Google_Signer',
 			'yoast_google_p12signer'       => 'google/auth/Google_P12Signer',
-			'yoast_google_batchrequest'    => 'google/service/Google_BatchRequest',
-			'yoast_google_uritemplate'     => 'google/external/URITemplateParser',
-			'yoast_google_auth'            => 'google/auth/Google_Auth',
-			'yoast_google_cache'           => 'google/cache/Google_Cache',
-			'yoast_google_io'              => 'google/io/Google_IO',
-			'yoast_google_mediafileupload' => 'google/service/Google_MediaFileUpload',
 			'yoast_google_authnone'        => 'google/auth/Google_AuthNone',
 			'yoast_google_oauth2'          => 'google/auth/Google_OAuth2',
 			'yoast_google_verifier'        => 'google/auth/Google_Verifier',
 			'yoast_google_loginticket'     => 'google/auth/Google_LoginTicket',
-			'yoast_google_utils'           => 'google/service/Google_Utils',
 			'yoast_google_pemverifier'     => 'google/auth/Google_PemVerifier',
-
-			// Caching
-			'yoast_google_filecache'       => 'google/cache/Google_FileCache',
-			'yoast_google_memcachecache'   => 'google/cache/Google_MemcacheCache',
-			'yoast_google_cacheparser'     => 'google/io/Google_CacheParser',
+			'yoast_google_model'           => 'google/service/Google_Model',
+			'yoast_google_service'         => 'google/service/Google_Service',
+			'yoast_google_serviceresource' => 'google/service/Google_ServiceResource',
+			'yoast_google_utils'           => 'google/service/Google_Utils',
+			'yoast_google_batchrequest'    => 'google/service/Google_BatchRequest',
+			'yoast_google_mediafileupload' => 'google/service/Google_MediaFileUpload',
+			'yoast_google_uritemplate'     => 'google/external/URITemplateParser',
+			'yoast_google_cache'           => 'google/cache/Google_Cache',
 
 			// Requests
+			'yoast_google_io'              => 'google/io/Google_IO',
 			'yoast_google_httprequest'     => 'google/io/Google_HttpRequest',
-			'yoast_google_httpstream_io'   => 'google/io/Google_HttpStreamIO',
 			'yoast_google_rest'            => 'google/io/Google_REST',
 
 			// Wordpress
 			'yoast_google_wpio'            => 'google/io/Google_WPIO',
 			'yoast_google_wpcache'         => 'google/cache/Google_WPCache',
-
-			// REPLACE ME!
-			'yoast_google_curlio'          => 'google/io/Google_CurlIO',
 		);
 
 		if ( ! empty( $oauth_files[$class_name] ) ) {
